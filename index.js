a2f99e187005891d90915cdfb52f7419a75e2701
Harsh Jain <harshjain0461@gmail.com>
2024-05-07 23:57:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ const port = process.env.PORT;
 
 try {
     connectToMongodb();
-    app.use(cors())
+    app.use(cors({
+        origin: `http://localhost:3000`,
+    }))
     app.use(express.json()) 
     app.use("/api/user", require("./routes/auth"))
     app.use("/api/watchlist", require("./routes/watchlist"))
