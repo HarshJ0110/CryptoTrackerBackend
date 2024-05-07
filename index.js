89e530badd6f27d925ee890b5f60af36dd39a6fc
Harsh Jain <harshjain0461@gmail.com>
2024-05-08 00:47:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,6 @@
 //     console.log(error);
 // }
 
-
 require('dotenv').config();
 const express = require("express");
 const app = express();
@@ -43,25 +42,23 @@ const connectToMongodb = require('./db');
 const port = process.env.PORT;
 
 // CORS middleware
-app.use((req, res, next) => {
+function setCorsHeaders(req, res, next) {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Origin', 'https://crypto-tracker-frontend-indol.vercel.app');
     res.setHeader('Access-Control-Allow-Credentials', true);
     if (req.method === 'OPTIONS') {
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-        res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-        res.setHeader('Access-Control-Allow-Origin', 'https://crypto-tracker-frontend-indol.vercel.app');
-        res.setHeader('Access-Control-Allow-Credentials', true);
         res.sendStatus(200);
     } else {
         next();
     }
-});
+}
+
+app.use(setCorsHeaders);
 
 try {
     connectToMongodb();
-    app.use(express.json()) ;
+    app.use(express.json());
     app.use("/api/user", require("./routes/auth"));
     app.use("/api/watchlist", require("./routes/watchlist"));
     app.listen(port, () => {
