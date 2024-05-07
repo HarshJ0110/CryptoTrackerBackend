1b5dc8bff507791607c4b8f7426dce02a35fe613
Harsh Jain <harshjain0461@gmail.com>
2024-05-08 00:42:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,48 @@
-require('dotenv').config()
+// require('dotenv').config()
+// const express = require("express");
+// const app = express();
+// const connectToMongodb = require('./db');
+// const cors = require('cors')
+
+// const port = process.env.PORT;
+
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//     res.setHeader('Access-Control-Allow-Origin', 'https://crypto-tracker-frontend-indol.vercel.app');
+//     res.setHeader('Access-Control-Allow-Credentials', true);
+//     if (req.method === 'OPTIONS') {
+//         res.sendStatus(200);
+//     } else {
+//         next();
+//     }
+// });
+
+// try {
+//     connectToMongodb();
+//     app.use(cors({
+//         origin: `https://crypto-tracker-frontend-indol.vercel.app`,
+//         credentials: true
+//     }))
+//     app.use(express.json()) 
+//     app.use("/api/user", require("./routes/auth"))
+//     app.use("/api/watchlist", require("./routes/watchlist"))
+//     app.listen(port, () => {
+//         console.log("Connected to server")
+//     })
+// } catch (error) {
+//     console.log(error);
+// }
+
+
+require('dotenv').config();
 const express = require("express");
 const app = express();
 const connectToMongodb = require('./db');
-const cors = require('cors')
 
 const port = process.env.PORT;
 
+// CORS middleware
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
@@ -20,18 +57,15 @@ app.use((req, res, next) => {
 
 try {
     connectToMongodb();
-    app.use(cors({
-        origin: `https://crypto-tracker-frontend-indol.vercel.app`,
-        credentials: true
-    }))
-    app.use(express.json()) 
-    app.use("/api/user", require("./routes/auth"))
-    app.use("/api/watchlist", require("./routes/watchlist"))
+    app.use(express.json()) ;
+    app.use("/api/user", require("./routes/auth"));
+    app.use("/api/watchlist", require("./routes/watchlist"));
     app.listen(port, () => {
-        console.log("Connected to server")
-    })
+        console.log("Connected to server");
+    });
 } catch (error) {
     console.log(error);
 }
+
 
 
