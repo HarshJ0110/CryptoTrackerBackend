1c0e8b0f7e19ec6b36df344a05996540c092b11a
Harsh Jain <harshjain0461@gmail.com>
2024-05-08 00:39:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,18 @@ const cors = require('cors')
 
 const port = process.env.PORT;
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Origin', 'https://crypto-tracker-frontend-indol.vercel.app');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    if (req.method === 'OPTIONS') {
+        res.sendStatus(200);
+    } else {
+        next();
+    }
+});
+
 try {
     connectToMongodb();
     app.use(cors({
