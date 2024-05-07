9a2646169fae478597395f3e5482426154ad5365
Harsh Jain <harshjain0461@gmail.com>
2024-05-08 00:45:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -49,6 +49,10 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'https://crypto-tracker-frontend-indol.vercel.app');
     res.setHeader('Access-Control-Allow-Credentials', true);
     if (req.method === 'OPTIONS') {
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+        res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        res.setHeader('Access-Control-Allow-Origin', 'https://crypto-tracker-frontend-indol.vercel.app');
+        res.setHeader('Access-Control-Allow-Credentials', true);
         res.sendStatus(200);
     } else {
         next();
@@ -66,6 +70,3 @@ try {
 } catch (error) {
     console.log(error);
 }
-
-
-
