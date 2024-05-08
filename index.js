978dc3e2015ce04f5f33224cab1458153043bc0e
Harsh Jain <harshjain0461@gmail.com>
2024-05-08 11:55:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,68 +1,24 @@
-// require('dotenv').config()
-// const express = require("express");
-// const app = express();
-// const connectToMongodb = require('./db');
-// const cors = require('cors')
-
-// const port = process.env.PORT;
-
-// app.use((req, res, next) => {
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-//     res.setHeader('Access-Control-Allow-Origin', 'https://crypto-tracker-frontend-indol.vercel.app');
-//     res.setHeader('Access-Control-Allow-Credentials', true);
-//     if (req.method === 'OPTIONS') {
-//         res.sendStatus(200);
-//     } else {
-//         next();
-//     }
-// });
-
-// try {
-//     connectToMongodb();
-//     app.use(cors({
-//         origin: `https://crypto-tracker-frontend-indol.vercel.app`,
-//         credentials: true
-//     }))
-//     app.use(express.json()) 
-//     app.use("/api/user", require("./routes/auth"))
-//     app.use("/api/watchlist", require("./routes/watchlist"))
-//     app.listen(port, () => {
-//         console.log("Connected to server")
-//     })
-// } catch (error) {
-//     console.log(error);
-// }
-
-require('dotenv').config();
+require('dotenv').config()
 const express = require("express");
 const app = express();
 const connectToMongodb = require('./db');
+const cors = require('cors')
 
 const port = process.env.PORT;
 
-// CORS middleware
-function setCorsHeaders(req, res, next) {
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Origin', 'https://crypto-tracker-frontend-indol.vercel.app');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    if (req.method === 'OPTIONS') {
-        res.sendStatus(200);
-    } else {
-        next();
-    }
+try {
+    connectToMongodb();
+    app.use(cors({
+        origin : `https://crypto-tracker-frontend-indol.vercel.app/`
+    }))
+    app.use(express.json()) 
+    app.use("/api/user", require("./routes/auth"))
+    app.use("/api/watchlist", require("./routes/watchlist"))
+    app.listen(port, () => {
+        console.log("Connected to server")
+    })
+} catch (error) {
+    console.log(error);
 }
 
-// Apply the CORS middleware to all requests
-app.use(setCorsHeaders);
 
-// Parse JSON request bodies
-app.use(express.json());
-
-app.use("/api/user", require("./routes/auth"));
-app.use("/api/watchlist", require("./routes/watchlist"));
-
-app.listen(port, () => {
-    console.log("Connected to server");
-});
