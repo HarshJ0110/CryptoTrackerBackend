55f9f63e9501b6febcab86e1f34af39b9de4b902
Harsh Jain <harshjain0461@gmail.com>
2024-05-08 12:51:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT;
 
 connectToMongodb();
 app.use(cors({
-    origin: ["http://localhost:3000" , process.env.FRONTEND_URL],
+    origin: ["http://localhost:3000" , process.env.FRONTEND_URL]
 }));
 
 
