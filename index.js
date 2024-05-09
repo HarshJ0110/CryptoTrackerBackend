94f2c75fc28a2acd4a5aaaad850c235434a6c0ce
Harsh Jain <harshjain0461@gmail.com>
2024-05-09 23:06:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,49 @@
-require('dotenv').config()
+// require('dotenv').config()
+// const express = require("express");
+// const app = express();
+// const connectToMongodb = require('./db');
+// const cors = require('cors')
+
+// const port = process.env.PORT;
+
+// connectToMongodb();
+
+
+// app.use(cors(
+//   origin: *
+// ));
+
+// app.use(express.json()) 
+// app.use("/api/user", require("./routes/auth"))
+// app.use("/api/watchlist", require("./routes/watchlist"))
+// app.listen(port, () => {
+//     console.log("Connected to server")
+// })
+
+
+require('dotenv').config();
 const express = require("express");
-const app = express();
+const cors = require('cors');
 const connectToMongodb = require('./db');
-const cors = require('cors')
+const authRoutes = require("./routes/auth");
+const watchlistRoutes = require("./routes/watchlist");
 
-const port = process.env.PORT;
+const app = express();
+const port = process.env.PORT || 3000; 
 
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB
 connectToMongodb();
 
-// Allow CORS for specific origin
-const allowedOrigin = 'https://crypto-tracker-frontend-indol.vercel.app/';
-const corsOptions = {
-  origin: allowedOrigin
-};
+// Routes
+app.use("/api/user", authRoutes);
+app.use("/api/watchlist", watchlistRoutes);
 
-app.use(cors(corsOptions));
-
-app.use(express.json()) 
-app.use("/api/user", require("./routes/auth"))
-app.use("/api/watchlist", require("./routes/watchlist"))
+// Server initialization
 app.listen(port, () => {
-    console.log("Connected to server")
-})
+    console.log(`Server is running on port ${port}`);
+});
+
