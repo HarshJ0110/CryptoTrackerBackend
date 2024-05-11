b388ef7d4cb1a096709782ec50bdf76f42b0ccff
Harsh Jain <harshjain0461@gmail.com>
2024-05-12 01:20:29 +0530

    Update fetchUser.js

--- middleware/fetchUser.js
+++ middleware/fetchUser.js
@@ -4,10 +4,12 @@ const User = require("../models/user")
 const fetchUser = async (req,res,next) => {
     const token = req.header("token");
     console.log(token);
-    if(token === null){
+    if(!token){
+        console.log("error in token")
         res.status(401).json({error: "Please Authenticate using valid token token not present"})
     }
     try{
+        console.log("in try");
         const data = jwt.verify(token, process.env.JWT_Secret)
         req.user = await User.findById(data.user.id).select("-password");
         console.log(req.user);
